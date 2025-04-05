a89292e4fa11cf442d849c0321b66dbaca0ed41d
R-Wolfcastle <eetss@leeds.ac.uk>
2025-04-05 22:47:38 +0100

    little test

--- scratch_test/expt/ppm_test.py
+++ scratch_test/expt/ppm_test.py
@@ -13,9 +13,9 @@ import matplotlib.cm as cm
 np.set_printoptions(precision=1, suppress=False, linewidth=np.inf)
 
 
-def make_ppm_step(u, dt, h_in, dx):
+def make_ppm_step(u, dt, dx, n):
     #assume u and h are colocated
-    n = u.shape[0]
+    #n = u.shape[0]
 
     def ppm_step(h):
         #follow steps laid out in:
@@ -24,6 +24,8 @@ def make_ppm_step(u, dt, h_in, dx):
         #and stuff learned from Colella and Woodward 1984.
 
         #Boundary condition is that h is prescribed at the lhs of the domain.
+        
+
 
 
         #STEP 1: 
@@ -34,7 +36,18 @@ def make_ppm_step(u, dt, h_in, dx):
         h_grad_lsd = jnp.zeros((n,))
         h_grad_rsd = jnp.zeros((n,))
 
-        h_grad_cd  = h_grad_cd.at[1:n-1].set((h[2:] - h[:n-1])/dx)
+
+#        #NOTE: THIS IS WITHOUT THE dx!!!!
+#        h_grad_cd  = h_grad_cd.at[1:n-1].set((h[2:] - h[:n-2])/(2))
+#        h_grad_cd  = h_grad_cd.at[-1].set(0)
+#        h_grad_cd  = h_grad_cd.at[0].set(0)
+#        h_grad_lsd = h_grad_lsd.at[1:].set((h[1:] - h[:n-1]))
+#        h_grad_lsd = h_grad_lsd.at[0].set(0)
+#        h_grad_rsd = h_grad_rsd.at[:n-1].set((h[1:] - h[:n-1]))
+#        h_grad_rsd = h_grad_rsd.at[-1].set(0)
+        
+        #NOTE: THIS IS WITH dx!!!!
+        h_grad_cd  = h_grad_cd.at[1:n-1].set((h[2:] - h[:n-2])/(2*dx))
         h_grad_cd  = h_grad_cd.at[-1].set(0)
         h_grad_cd  = h_grad_cd.at[0].set(0)
         h_grad_lsd = h_grad_lsd.at[1:].set((h[1:] - h[:n-1])/dx)
@@ -46,57 +59,131 @@ def make_ppm_step(u, dt, h_in, dx):
         h_grad_lsd_mag = jnp.abs(h_grad_lsd)
         h_grad_rsd_mag = jnp.abs(h_grad_rsd)
 
-        min_mag_grads_cd_lsd = jnp.where(h_grad_cd_mag < h_grad_lsd_mag, h_grad_cd, h_grad_lsd)
-        min_mag_grads_cd_rsd = jnp.where(h_grad_cd_mag < h_grad_rsd_mag, h_grad_cd, h_grad_rsd)
-        min_mag_grads = jnp.where(min_mag_grads_cd_lsd < min_mag_grads_cd_rsd,\
+        min_mag_grads_cd_lsd = jnp.where(h_grad_cd_mag <= h_grad_lsd_mag, h_grad_cd, h_grad_lsd)
+        min_mag_grads_cd_rsd = jnp.where(h_grad_cd_mag <= h_grad_rsd_mag, h_grad_cd, h_grad_rsd)
+        min_mag_grads = jnp.where(min_mag_grads_cd_lsd <= min_mag_grads_cd_rsd,\
                                    min_mag_grads_cd_lsd, min_mag_grads_cd_rsd)
 
-        h_r = jnp.zeros_like((n-1,))
-        h_l = jnp.zeros_like((n-1,))
-        h_r = h_r.at[:].set(h[:n-1]+min_mag_grads[:n-1]*0.5*dx)
-        h_l = h_l.at[:].set(h[1:]-min_mag_grads[1:]*0.5*dx)
 
+        #print(min_mag_grads)
 
 
         #STEP 2:
         #First guess at face-centred values by fitting cubic to grads and values
-        h_ls = jnp.zeros((n-1,))
-        h_ls = h_ls.at[:].set( (1/2)*(h[:n-1]+h[1:]) - (1/6)*(min_mag_grads[1:]-min_mag_grads[:n-1]) )
+        h_ls = jnp.zeros((n,))
+        h_ls = h_ls.at[1:].set( (1/2)*(h[:n-1]+h[1:]) - (1/6)*(min_mag_grads[1:]*dx - min_mag_grads[:n-1]*dx) )
 
-        h_rs = h_ls.copy()
 
+        h_rs = jnp.zeros((n,))
+        h_rs = h_rs.at[:n-1].set(h_ls[1:])
+        
+        h_ls = h_ls.at[0].set(h[0]-0.5*(h_rs[0]-h[0]))
+        h_rs = h_rs.at[-1].set(h[-1]+0.5*(h[-1]-h_rs[-1]))
+        #Note, for these fields, each h as an h_l and an h_r, but the h_l for the first
+        #grid point and the h_r for the last have been modified so that the linear interps
+        #just give h in the centre. But I'm not sure what do do about it. Think more carefully.
+
+
+
+#        print(h_ls)
 
 
         #STEP 3:
         #STEEPENING! Let's ignore this for now :)
 
 
+
+
+
+
+
+
         #STEP 4:
-        h_six = jnp.zeros((n,))
-        h_six = h_six.at[0].set(6*h[0])
-        h_six = h_six.at[-1].set(6*h[-1])
-        h_six = h_six.at[1:n-1].set(6*(h[1:n-1] - 0.5*(h_ls[:n-2] + h_rs[1:n-1])))
+        #Adjust the L and R points so that there are no overshoots (local
+        #extrema) in the parabolas for each cell.
+
+        h_six = 6*( h - 0.5*( h_ls + h_rs ) )
+        delta_h = ( h_rs - h_ls )
+
+        #print(h_six)
+        #print(delta_h)
+        #raise
+
+        condition_0 = jnp.where((jnp.abs(delta_h) < jnp.abs(h_six)), 1, 0)
+        condition_1 = jnp.where((condition_0==1) & (( h_rs - h )*( h - h_ls ) < 0), 1, 0)
+        condition_2 = jnp.where((condition_0==1) & ( delta_h**2 < (delta_h*h_six)), 1, 0)
+        condition_3 = jnp.where((condition_0==1) & (-delta_h**2 > (delta_h*h_six)), 1, 0)
+#        print(condition_0)
+#        print(condition_1)
+#        print(condition_2)
+#        print(condition_3)
+#        raise
+
+        condition_2 = jnp.where(((condition_2==1) & (condition_3==0) & (condition_1==0)), 1, 0)
+        condition_3 = jnp.where(((condition_3==1) & (condition_2==0) & (condition_1==0)), 1, 0)
+
+        #TODO: check whether conditions are true at boundaries and implications!
+
+
+#        print(condition_1)
+#        print(condition_2)
+#        print(condition_3)
+#        raise
+
+        #where condition 1, we have an extremum, so set hl and hr to h
+        #to make the thing flat.
+        h_ls = jnp.where(condition_1==1, h, h_ls)
+        h_rs = jnp.where(condition_1==1, h, h_rs)
+
+        #where condition 2, there is an overshoot close to the right-hand side
+        #of the cell, so h_l should be adjusted so that the gradient of h(x)
+        #is zero at the right-hand side of the cell (x=1):
+        h_ls = jnp.where(condition_2==1, 3*h - 2*h_rs, h_ls)
+
+        #where condition 3, other way round
+        h_rs = jnp.where(condition_3==1, 3*h - 2*h_ls, h_rs)
+
+
+#        print(h_ls)
+#        raise
+
+
+
         
-        delta_h = jnp.zeros((n,))
-        delta_h = delta_h.at[1:n-1].set((h_rs[:n-2] - h_ls[1:n-1])/dx)
-        delta_h = delta_h.at[0].set(0)
-        delta_h = delta_h.at[-1].set(0)
+        #STEP 5:
+        #COMPUTE THE UPDATES TO h!!
+        y = u * dt / dx
+        h_out = h_rs - 0.5*y*(delta_h - h_six*(1 - (2/3)*y))
+        h_in  = h_out.copy()
+        h_in  = h_in.at[1:].set(h_out[:n-1])
+
+        h_new = h - y*(h_out - h_in)
 
 
-        condition_1 = jnp.where(( delta_h**2 < (delta_h*h_six)), 1, 0)
-        condition_2 = jnp.where((-delta_h**2 < (delta_h*h_six)), 1, 0)
-        #TODO: LEFT THINGS HERE FOR THE WEEKEND, WORK OUT WTF IS GOING ON
-        condition_3 = jnp.where(((h_rs[])*() < 0), 1, 0)
-        #TODO: I've realised this is a bad way of doing things. Each h point
-        #should have a h_ls and h_rs even if the [0] for l and [-1] for r
-        #are just placeholders. It just makes things easier. Change in the code!!!
-
-
-
-
-
-       return h_new
+        return h_new
 
     return ppm_step
 
 
+
+def plot_h(h, i):
+    plt.plot(h)
+    plt.ylim(-0.5,1.5)
+    plt.savefig("../misc/advec_tests/ppm_test_1_{}.png".format(i))
+    plt.close()
+
+
+x = jnp.linspace(0,1,50)
+dt = 2e-1
+u = 0.02
+
+#h_init = 1 - 0.5*(1+ jnp.tanh(100*(x-0.5)))
+h_init = 0.5*(1+ jnp.tanh(100*(x-0.25)))
+
+
+ppm_step = make_ppm_step(u, dt, x[1]-x[0], h_init.shape[0])
+
+h_i = h_init.copy()
+for i in range(100):
+    plot_h(h_i, i)
+    h_i = ppm_step(h_i) 
